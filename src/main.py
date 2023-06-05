ec489a29fd655513cfbbcb79d5805769fc605a2a
furkansancu <furkansancu@outlook.com>
2023-06-05 20:11:19 +0300

    new theme added, dnd added, consistency functional

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
-import pathlib, os
-import tkinter, ttkthemes, pytesseract
-from tkinter import filedialog, messagebox, ttk, font
+import pathlib, os, filetype, requests
+import tkinter, customtkinter, tkinterdnd2, pytesseract
+from tkinter import filedialog, messagebox, ttk
 from PIL import Image, ImageTk
+from urllib.parse import urlparse
+from io import BytesIO
 
 import components.util as util
 import components.mem as mem
@@ -9,12 +11,16 @@ import components.mem as mem
 utils = util.Util()
 memory = mem.MemoryHandler()
 
-def SetTesseractPath(path):
-    pytesseract.pytesseract.tesseract_cmd = path
+class OT_TK (customtkinter.CTk, tkinterdnd2.TkinterDnD.DnDWrapper):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.TkdndVersion = tkinterdnd2.TkinterDnD._require(self)
+
+customtkinter.set_appearance_mode("dark")
 
 class NoTesseract:
     def __init__(self):
-        self.window = ttkthemes.ThemedTk(theme="arc")
+        self.window = OT_TK()
         self.window.geometry("376x512")
         self.window.iconbitmap("src/images/icon.ico")
         self.window.resizable(False, False)
@@ -57,7 +63,7 @@ class NoTesseract:
                     message="Selected file is not a tesseract-ocr file. Please select correct tesseract-ocr file."
                 )
             else:
-                SetTesseractPath(self.tesseractLocation)
+                pytesseract.pytesseract.tesseract_cmd = self.tesseractLocation
                 self.Close()
 
     def Close(self):
@@ -66,7 +72,7 @@ class NoTesseract:
 
 class OCRTool:
     def __init__ (self) :
-        self.window = ttkthemes.ThemedTk(theme="arc")
+        self.window = OT_TK()
         self.window.geometry("376x616")
         self.window.iconbitmap("src/images/icon.ico")
         self.window.resizable(False, False)
@@ -78,34 +84,34 @@ class OCRTool:
         self.window.mainloop()
 
     def DnDArea(self):
-        self.dndFrame = tkinter.Frame(self.window, width=360, height=180)
-        self.dndFrame.configure(bg="#fff")
+        self.dndFrame = customtkinter.CTkFrame(self.window, width=360, height=180)
         self.dndFrame.pack_propagate(False)
         self.dndFrame.place(x=8, y=8)
-        self.dndFrameButton = ttk.Button(
+        self.dndFrameButton = customtkinter.CTkButton(
             self.dndFrame,
             text="Drag and Drop Image or Select File",
             cursor="hand2",
             command=self.SelectImageFile
         )
         self.dndFrameButton.pack(fill=tkinter.BOTH, expand=True)
+        self.dndFrameButton.drop_target_register(tkinterdnd2.DND_ALL)
+        self.dndFrameButton.dnd_bind("<<Drop>>", self.DragAndDropImage)
 
     def ConsistencyArea(self):
-        self.consistency = tkinter.IntVar(value=180)
-        self.consFrame = tkinter.Frame(self.window, width=360, height=32)
-        self.consFrame.configure(bg="white")
+        self.consistency = tkinter.IntVar(value=215)
+        self.consFrame = customtkinter.CTkFrame(self.window, width=360, height=32)
         self.consFrame.place(x=8, y=196)
-        self.consFrameTextFont = font.Font(family="Ubuntu Medium", size=10, weight="bold")
-        self.consFrameText = ttk.Label(self.consFrame, text="Consistency: ", font=self.consFrameTextFont, background="white")
-        self.consFrameText.place(x=8, y=8)
-        self.consFrameSlider = ttk.Scale(self.consFrame, from_=40, to=255, length=256, variable=self.consistency)
+        self.consFrameTextFont = customtkinter.CTkFont(family="Ubuntu Medium", size=12, weight="bold")
+        self.consFrameText = customtkinter.CTkLabel(self.consFrame, text="Consistency: ", font=self.consFrameTextFont)
+        self.consFrameText.place(x=8, y=4)
+        self.consFrameSlider = customtkinter.CTkSlider(self.consFrame, from_=50, to=255, variable=self.consistency, width=256)
         self.consFrameSlider.place(x=96, y=10)
 
     def OutputArea(self):
-        self.optFrame = tkinter.Frame(self.window, width=360, height=372)
+        self.optFrame = customtkinter.CTkFrame(self.window, width=360, height=372)
         self.optFrame.place(x=8, y=236)
         self.optFrame.pack_propagate(False)
-        self.optText = tkinter.Text(self.optFrame)
+        self.optText = customtkinter.CTkTextbox(self.optFrame)
         self.optText.pack(fill=tkinter.BOTH, expand=True, ipadx=8, ipady=8)
 
     def SelectImageFile(self):
@@ -118,11 +124,31 @@ class OCRTool:
             )
         )
 
-        if self.imageFile != None:
+        if len(self.imageFile) > 0:
             self.SetResult(self.imageFile)
+
+    def DragAndDropImage(self, event):
+        isLink = len(urlparse(event.data).netloc) > 0
+        if (isLink):
+            response = requests.get(event.data)
+            img = Image.open(BytesIO(response.content))
+            self.SetResult(img)
+        else:
+            localurl = event.data[1:-1]
+            if filetype.is_image(localurl):
+                self.SetResult(localurl)
+            else:
+                messagebox.showerror(
+                    title="Selected file is not a image file.",
+                    message="Selected file is not a image file. Please select a image file to run OCR."
+                )
             
     def SetResult(self, image):
-        result = pytesseract.image_to_string(image=image)
+        image_file = Image.open(image)
+        image_file = image_file.convert('L')
+        image_file = image_file.point( lambda p: 255 if p > self.consistency.get() else 0 )
+        image_file = image_file.convert('1')
+        result = pytesseract.image_to_string(image=image_file)
         self.optText.delete("1.0", "end")
         self.optText.insert(tkinter.END, result)
 
@@ -135,114 +161,7 @@ if __name__ == '__main__':
         memory_tesseract_path = memory.GetData("tesseract_path")
 
     if tesseract_located:
-        SetTesseractPath(memory_tesseract_path)
+        pytesseract.pytesseract.tesseract_cmd = memory_tesseract_path
         OCRTool()
     else:
         NoTesseract()
-
-# def resourcePath(relative_path):
-#     base_path = getattr(sys, '_MEIPASS', path.dirname(path.abspath(__file__)))
-#     return path.join(base_path, relative_path)
-
-# class OCRTOOL:
-#     def __init__(self, root):
-#         self.window = root
-#         if (tesseract_found == False): self.SetOCRWindow()
-#         self.consistency = IntVar(value=100)
-#         self.SetOnTop( self.window )
-#         self.SetOnCenter( self.window )
-#         # self.dndImg = ImageTk.PhotoImage(file=resourcePath("pictures/dnd.png"))
-#         # self.lb = Label(self.window, image=self.dndImg, width= 301, cursor="hand2", background="#dce4e2")
-#         # self.lb.drop_target_register(DND_FILES)
-#         # self.lb.dnd_bind('<<Drop>>', lambda e: self.UpdateFilePath(e.data))
-#         # self.lb.bind("<Button-1>", lambda e: self.SetFilePath())
-#         # self.lb.pack(side= TOP, ipady=16, ipadx=0)
-#         self.cn = ttk.Scale(self.window, from_=20, to=235, orient= HORIZONTAL, length=301, variable=self.consistency)
-#         self.cn.pack(side= TOP, expand= True, pady=6)
-#         self.result = Text(self.window, height=14, width=40)
-#         self.result.pack(side= TOP, expand=TRUE)
-            
-#     def SetOnTop(self, window):
-#         window.attributes('-topmost', True)
-#         window.update()
-#         window.attributes('-topmost', False)
-
-#     def SetOnCenter(self, window):
-#         w = window.winfo_width()
-#         h = window.winfo_height()
-#         ws = window.winfo_screenwidth()
-#         hs = window.winfo_screenheight()
-#         x = (ws/2) - (w/2)
-#         y = (hs/2) - (h/2)
-#         window.geometry('+%d+%d'%(x, y))
-
-#     def SetOCRWindow(self):
-#         self.window.withdraw()
-#         self.top= Toplevel(self.window)
-#         self.top.protocol("WM_DELETE_WINDOW", self.TopLevelClosed)
-#         self.top.geometry("444x275")
-#         self.top.title("OCR Tool")
-#         self.top['background'] = "#dce4e2"
-#         self.top.resizable(False, False)
-#         self.top.lbl0 = Label(self.top, text="", background="#dce4e2").pack(side=TOP, pady=8)
-#         self.top.lbl1 = Label(self.top, text="This program uses Tesseract OCR and to use that program").pack(side= TOP)
-#         self.top.lbl2 = Label(self.top, text="It is essentail to have Tesseract OCR installed on your computer.").pack(side= TOP)
-#         self.top.lbl3 = Label(self.top, text="If you have not installed it, please do it via this link:").pack(side= TOP)
-#         self.top.lbl4 = Label(self.top, text="https://github.com/UB-Mannheim/tesseract/wiki", foreground="#0000FF", cursor="hand2")
-#         self.top.lbl4.bind("<Button-1>", lambda e: callback("https://github.com/UB-Mannheim/tesseract/wiki"))
-#         self.top.lbl4.pack(side= TOP, pady=8)
-#         self.top.lbl5 = Label(self.top, text="If you already have Tesseract OCR installed on your computer,").pack(side= TOP)
-#         self.top.lbl6 = Label(self.top, text="Please locate it:").pack(side= TOP)
-#         helv = tkFont.Font(family='Helvetica', size=8, weight='bold')
-#         self.top.btn = Button(self.top, text="Locate Tesseract OCR", font=helv, cursor="hand2", command=self.SetOCRPath).pack(side= TOP, pady=16, padx=32, ipadx=8, ipady=4)
-#         self.SetOnCenter(self.top)
-#         self.SetOnTop(self.top)
-
-#     def TopLevelClosed (self):
-#         exit(1)
-
-#     def SetOCRPath (self):
-#         global tesseract_found
-#         stored_data["tesseract_path"] = filedialog.askopenfilename(
-#             initialdir= environ['PROGRAMFILES']+"\\Tesseract-OCR",
-#             title = "Select tesseract.exe",
-#             filetypes=(
-#                 ("tesseract.exe", "tesseract.exe"),
-#                 ("any file", "*.*")
-#             )
-#         )
-#         if(stored_data["tesseract_path"] != ""):
-#             tesseract_found = checkTesseractPath(stored_data["tesseract_path"])
-#             if (tesseract_found == False):
-#                 messagebox.showerror(title="Wrong file", message="Selected file is not running correctly.")
-#             else:
-#                 pickle.dump({"tesseract_path": stored_data["tesseract_path"]}, open( settings_filepath, "wb" ))
-#                 self.window.deiconify()
-#                 self.top.withdraw()
-
-#     def SetFilePath(self):
-#         self.window.filepath = filedialog.askopenfilename(
-#             initialdir= Path.home() / "Downloads",
-#             title = "Select Image",
-#             filetypes=(
-#                 ("image files", ("*.jpg", "*.jpeg", "*.png")),
-#                 ("jpeg files", ("*.jpg", "*.jpeg")),
-#                 ("png files", "*.png")
-#             )
-#         )
-#         self.FilePathUpdated()
-
-#     def UpdateFilePath (self, path):
-#         self.window.filepath = path[1:-1]
-#         self.FilePathUpdated()
-
-#     def FilePathUpdated (self):
-#         if (self.window.filepath != None):
-#             self.ScanImage()
-            
-#     def ScanImage (self):
-#         image_file = Image.open(self.window.filepath)
-#         image_file = image_file.point( lambda p: 255 if p > self.consistency.get() else 0 )
-#         text = pytesseract.image_to_string(image_file)
-#         self.result.delete("1.0", "end")
-#         self.result.insert(END, text)
